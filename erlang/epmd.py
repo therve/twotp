14cf8530dafc4eb294fa4c865437fa5e77c67003
th <th@wasabox>
2008-03-08 12:40:17 +0100

    Basic twistd plugin

--- erlang/epmd.py
+++ erlang/epmd.py
@@ -11,6 +11,8 @@ import struct
 from twisted.internet.protocol import Protocol, ClientFactory
 from twisted.internet.defer import Deferred, succeed
 from twisted.python import log
+from twisted.application.service import Service
+from twisted.application.internet import TCPClient, TCPServer
 
 from erlang.parser import theParser
 from erlang.packer import thePacker
@@ -448,3 +450,28 @@ class OneShotPortMapperFactory(ClientFactory):
         self.oneShotConnection()
         return d
 
+
+
+class PersistentPortMapperService(Service):
+    """
+    Persistent port mapper service to be used with twistd.
+    """
+
+    def __init__(self, methodsHolder, nodeName, cookie):
+        self.methodsHolder = methodsHolder
+        self.nodeName = nodeName
+        self.cookie = cookie
+
+
+    def startService(self):
+        Service.startService(self)
+        epmd = PersistentPortMapperFactory(self.nodeName, self.cookie)
+        epmd._connectDeferred = Deferred()
+        nodeFactory = epmd.nodeFactoryClass(self.methodsHolder, self.nodeName,
+                self.cookie, epmd._connectDeferred)
+        server = TCPServer(0, nodeFactory)
+        server.startService()
+        epmd.nodePortNumber = server._port.getHost().port
+        client = TCPClient(epmd.host, epmd.EPMD_PORT, epmd)
+        client.startService()
+        return epmd._connectDeferred
---
+++ twisted/plugins/node.py
@@ -0,0 +1,51 @@
+# Copyright (c) 2007-2008 Thomas Herve <therve@free.fr>.
+# See LICENSE for details.
+
+"""
+Twisted erlang node.
+"""
+
+
+from zope.interface import implements
+
+from twisted.plugin import IPlugin
+from twisted.application.service import IServiceMaker
+from twisted.python.usage import Options
+from twisted.python.reflect import namedAny
+
+from erlang.epmd import PersistentPortMapperService
+from erlang import readCookie, buildNodeName
+
+
+
+class NodeOptions(Options):
+    optParameters = [
+        ['nodename', 'n', 'twisted', 'Name of the node'],
+        ['cookie', 'c', '', 'Erlang cookie value'],
+        ['methods', 'm', '', 'Methods holder']
+    ]
+
+
+
+class NodePlugin(object):
+    implements(IPlugin, IServiceMaker)
+
+    tapname = "twotp"
+    description = "twotp erlang node"
+    options = NodeOptions
+
+
+    def makeService(self, options):
+        cookie = options['cookie']
+        if not cookie:
+            cookie = readCookie()
+        methodsHolder = options.get('methods', {})
+        if methodsHolder:
+            methodsHolder = namedAny(methodsHolder)
+        nodeName = buildNodeName(options['nodename'])
+        epmd = PersistentPortMapperService(methodsHolder, nodeName, cookie)
+        return epmd
+
+
+
+nodePlugin = NodePlugin()
