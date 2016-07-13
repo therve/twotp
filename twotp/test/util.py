9cc1670b29753ea6b9643c0ee56805c51b7d681f
Thomas Herve <therve@redhat.com>
2016-07-13 14:03:31 +0200

    Remove assertIsInstance compat code

--- twotp/test/util.py
+++ twotp/test/util.py
@@ -14,21 +14,3 @@ class TestCase(TrialTestCase):
     Specific TestCase class to add some specific functionalities or backport
     recent additions.
     """
-    if getattr(TrialTestCase, 'assertIsInstance', None is None):
-        def failUnlessIsInstance(self, instance, classOrTuple):
-            """
-            Assert that the given instance is of the given class or of one of
-            the given classes.
-
-            @param instance: the object to test the type (first argument of the
-                C{isinstance} call).
-            @type instance: any.
-            @param classOrTuple: the class or classes to test against (second
-                argument of the C{isinstance} call).
-            @type classOrTuple: class, type, or tuple.
-            """
-            if not isinstance(instance, classOrTuple):
-                self.fail("%r is not an instance of %s" %
-                          (instance, classOrTuple))
-
-        assertIsInstance = failUnlessIsInstance
